92084f40b8ba099956a258720b042ea2ff85752d
Evgeniya Veselkova <evgeniyaveselk@gmail.com>
2021-03-01 21:04:11 -0500

    Toggle fixed-nav class

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
 
-function fixNav() {
-  
+function fixNav() { 
+  if (window.scrollY >= topOfNav ) {
+    document.body.classList.add('fixed-nav');  
+  } else {
+    document.body.classList.remove('fixed-nav');
+  }
 }
 
 window.addEventListener('scroll', fixNav);
